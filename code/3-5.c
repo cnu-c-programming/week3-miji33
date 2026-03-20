39aa5eb8d1ca4898c361be9407046982fa602376
miji33 <kidmiji@gmail.com>
2026-03-20 12:22:46 +0900

    1

--- code/3-5.c
+++ code/3-5.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+void my_sum(char T, int count, ...) {
+    va_list ap;
+    va_start(ap, T);
+
+    if (T == 'S') {
+        for (int i=0; i<count; i++) {
+            printf("%s ", va_arg(ap, char*));
+        }
+    } else if (T == 'C') {
+        for (int i=0; i<count; i++) {
+            printf("%c", (char)va_arg(ap, int));
+        }
+    } else if (T == 'D') {
+        int total = 0;
+        for (int i=0; i<count; i++) {
+            total += va_arg(ap, int);
+        }
+        printf("%d", total);
+    }
+
+    va_end(ap);
+    printf("\n");
+}
+
 int main() {
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C', 'N', 'U');
+    my_sum('D', 4, 10, 20, 30, 40);
 
     return 0;
 }
